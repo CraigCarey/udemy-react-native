a76a08121c1079c66b616f6aa00af6bd65489cb9
test <test@Craigs-MacBook.local>
2017-11-11 23:11:23 +0000

    Stripped out boilerplate

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+import React from 'react';
+import { AppRegistry, Text } from 'react-native';
+
+const App = () => {
+    return (
+        <Text>Some text</Text>
+    );
+};
 
 AppRegistry.registerComponent('albums', () => App);
