f811753dc4ee6d889bd54e5cd222e2a8023e0a76
Craig <sgccarey@gmail.com>
2017-11-17 14:39:43 +0000

    Auth: Added login/logout functionality

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
+
+    state = { loggedIn: null };
 
     // lifecycle method
     componentWillMount() {
@@ -16,14 +18,57 @@ export default class App extends Component {
                 storageBucket: 'auth-78c8a.appspot.com',
                 messagingSenderId: '519084563262'
             });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+
+        const { logOutContainerStyle, spinnerContainerStyle } = styles;
+
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <View style={logOutContainerStyle}>
+                        <Button onPress={ () => { firebase.auth().signOut(); }}>
+                            Log Out
+                        </Button>
+                    </View>
+                );
+            case false:
+                return <LoginForm />;
+            default:
+                return(
+                    <View style={spinnerContainerStyle}>
+                        <Spinner size='large'/>
+                    </View>
+                );
+        }
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         )
     }
 }
+
+const styles = {
+    logOutContainerStyle: {
+        height: 45
+    },
+    spinnerContainerStyle: {
+        height: 250,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+};
--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,15 +1,62 @@
 import React, { Component } from 'react';
-import { Button, Card, CardSection, Input } from "./common";
+import { Text } from 'react-native';
+import { Button, Card, CardSection, Input, Spinner } from "./common";
+import firebase from 'firebase';
 
 export default class LoginForm extends Component {
 
     state = {
         email: '',
-        password: ''
+        password: '',
+        error: '',
+        loading: false
     };
 
+    onButtonPress()
+    {
+        const { email, password } = this.state;
+
+        this.setState({ error: '', loading: true });
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(this.onLoginSuccess.bind(this))
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then(this.onLoginSuccess.bind(this))
+                    .catch(this.onLoginFail.bind(this));
+            });
+    }
+
+    onLoginSuccess() {
+        this.setState({
+            email: '',
+            password: '',
+            error: '',
+            loading: false
+        });
+    }
+
+    onLoginFail() {
+        this.setState({
+            error: 'Authentication Failed',
+            loading: false
+        });
+    }
+
+    renderButton() {
+        if (this.state.loading) {
+            return <Spinner size="small" />;
+        }
+
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                Login
+            </Button>
+        );
+    }
+
     render() {
-        return(
+        return (
             <Card>
 
                 <CardSection>
@@ -35,13 +82,23 @@ export default class LoginForm extends Component {
 
                 </CardSection>
 
+                <Text style={styles.errorTextStyle}>
+                    {this.state.error}
+                </Text>
+
                 <CardSection>
-                    <Button>
-                        Login
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
 
             </Card>
         );
     }
 }
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
