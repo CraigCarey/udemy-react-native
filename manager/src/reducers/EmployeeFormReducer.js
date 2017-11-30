714bba9db2f7910b508419cb05504a621a4f93f3
Craig <sgccarey@gmail.com>
2017-11-30 19:22:06 +0000

    Manager: Added Employee create form

---
+++ manager/src/reducers/EmployeeFormReducer.js
@@ -0,0 +1,18 @@
+import { EMPLOYEE_UPDATE } from '../actions/types';
+
+const INITIAL_STATE = {
+    name: '',
+    phone: '',
+    shift: ''
+};
+
+export default (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        case EMPLOYEE_UPDATE:
+            // action.payload === { prop: 'name' value: 'Jane' }
+            // [action.payload.prop] = name (through key interpolation)
+            return { ...state, [action.payload.prop]: action.payload.value };
+        default:
+            return state;
+    }
+}
